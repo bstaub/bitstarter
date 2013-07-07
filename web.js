17ff7f085381d5a0a79f5a3e61d822ac03788e27
Bruno Staub <bruno.staub@intersolution.ch>
2013-07-08 01:33:55 +0200

    test again

--- web.js
+++ web.js
@@ -3,12 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-fs = require('fs')
-fs.readFile('./index.html', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  response.send(data);
+
+ var k = fs.readFileSync('./index.html','utf8');
+  response.send(k);
+
+
 });
 
 var port = process.env.PORT || 5000;
