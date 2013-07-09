19bb4695c55d9ffdcf0f4299def2660c540495d0
Bruno Staub <bruno.staub@intersolution.ch>
2013-07-09 14:53:08 +0200

    grader.js and output hw3.3

--- grader.js
+++ grader.js
@@ -24,8 +24,12 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var sys = require('util');
 var HTMLFILE_DEFAULT = "index.html";
+var HTMLFILE_FROM_URL = "index_url.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var CHECKURL_DEFAULT = "http://aqueous-everglades-3380.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -34,7 +38,27 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+
 };
+
+
+var assertUrlExists = function(myurl) {
+
+rest.get(myurl).on('complete', function(result) {
+  if (result instanceof Error) {
+    sys.puts('Error: ' + result.message);
+    this.retry(5000); // try again after 5 sec
+  } else {
+
+     var outfile = HTMLFILE_FROM_URL;
+     var out = result;
+     fs.writeFileSync(outfile, out);  
+     return result;
+  }
+});
+
+};
+
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -55,6 +79,10 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var doNothingFn = function(result) { 	
+     console.log("this function does nothing"); 
+}  
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -62,13 +90,45 @@ var clone = function(fn) {
 };
 
 if(require.main == module) {
+
+     var outfile = HTMLFILE_FROM_URL;
+     var out = '';
+     fs.writeFileSync(outfile, out);
+
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url [url]', 'URL of the heroku Website', clone(assertUrlExists))
         .parse(process.argv);
+    
+/*
+    console.log('LOG url: ' + program.url);
+    console.log('LOG checks: ' + program.checks);
+    console.log('LOG file: ' + program.file);
+*/
+
+if(program.url) {
+
+setTimeout(function() {     
+    var checkJson = checkHtmlFile(HTMLFILE_FROM_URL, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}, 2000);
+
+} else {
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+}
+
+
+
+
+
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
+
+// ./grader.js --checks checks.json --url http://aqueous-everglades-3380.herokuapp.com
+// ./grader.js --checks checks.json --file index.html
